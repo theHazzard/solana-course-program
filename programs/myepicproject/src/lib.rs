fcf771d539367202423ae8f1d118cb55e6d40a60
Enzo Luis Strongoli <thehazzard@gmail.com>
2021-11-07 19:51:07 -0300

    try

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,14 +1,92 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Ac9EmrmiQWgs7hSLLALjcpeQsSpw9bHXPXAC7GawxKRM");
 
 #[program]
 pub mod myepicproject {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+  
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+
+    let item = ItemStruct {
+        gif_link: gif_link.to_string(),
+        user_address: *base_account.to_account_info().key,
+        votes: 0,
+    };
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
+  pub fn update_item(ctx: Context<UpdateItem>, gif_link: String) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+
+    let mut item = base_account.gif_list.iter_mut().find(|elem| elem.gif_link ==  gif_link).unwrap();
+
+    item.votes += 1;
+    Ok(())
+  }
+
+  pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> ProgramResult {
+    let ix = anchor_lang::solana_program::system_instruction::transfer(
+        &ctx.accounts.from.key(),
+        &ctx.accounts.to.key(),
+        amount,
+    );
+    anchor_lang::solana_program::program::invoke(
+        &ix,
+        &[
+            ctx.accounts.from.to_account_info(),
+            ctx.accounts.to.to_account_info(),
+        ],
+    )
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateItem<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Accounts)]
+pub struct SendSol<'info> {
+    #[account(mut)]
+    from: Signer<'info>,
+    #[account(mut)]
+    to: UncheckedAccount<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub votes: u32,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
+}
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,14 +1,60 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
-describe('myepicproject', () => {
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // You'll need to now pass a GIF link to the function!
+  await program.rpc.addGif("https://c.tenor.com/t_SMdNFvn5cAAAAC/fullmetal-alchemist-brotherhood-edward-elric.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
+  await program.rpc.updateItem("https://c.tenor.com/t_SMdNFvn5cAAAAC/fullmetal-alchemist-brotherhood-edward-elric.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
